b589361fb69e605a11b2fb3a6125d37ff690a062
Tran Van Hong Quan <hongquanhak@gmail.com>
2020-09-11 14:59:52 +0700

    create project

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <>
+            works
+        </>
+    );
+}
+
+export default Navbar;
